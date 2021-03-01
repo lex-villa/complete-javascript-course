f602204919b30965ab4dce03e16df9b5726cb774
Alejandro VIlla <alexandros17.sk@gmail.com>
2021-03-01 10:30:31 -0600

    exploring scope

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  printAge();
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
